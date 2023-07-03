fc9a16f28fd241223cff9e00bdc0c61b0fe4ab23
ES <ES@ES-KOMPUTEREMSZ>
2023-07-03 20:39:41 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static bool IsPrime(int number)
+    {
+        if (number < 2)
+            return false;
+
+        for (int i = 2; i <= Math.Sqrt(number); i++)
+        {
+            if (number % i == 0)
+                return false;
+        }
+
+        return true;
+    }
+
+    static void Main()
+    {
+        int count = 0;
+
+        for (int i = 0; i <= 100; i++)
+        {
+            if (IsPrime(i))
+                count++;
+        }
+
+        Console.WriteLine("Liczba liczb pierwszych w zakresie 0 - 100: " + count);
+    }
+}
